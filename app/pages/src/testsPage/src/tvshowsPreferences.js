971717499812424df11a67256321e3c928fb265b
Pavel_Datsenka <Pavel_Datsenka@epam.com>
2016-05-08 01:14:32 +0300

    test improvment

--- app/pages/src/testsPage/src/tvshowsPreferences.js
+++ app/pages/src/testsPage/src/tvshowsPreferences.js
@@ -4,9 +4,9 @@
 
     myApp.controller('tvshowsPreferencesController', tvshowsPreferencesController);
 
-    tvshowsPreferencesController.$inject = ['$scope', '$timeout', 'testDataService', 'CONSTANTS', '$uibModalInstance'];
+    tvshowsPreferencesController.$inject = ['$scope', '$timeout', 'testDataService', 'CONSTANTS', '$uibModalInstance', 'preferencesService'];
 
-    function tvshowsPreferencesController($scope, $timeout, testDataService, CONSTANTS, $uibModalInstance) {
+    function tvshowsPreferencesController($scope, $timeout, testDataService, CONSTANTS, $uibModalInstance, preferencesService) {
         $scope.lettersList = ['политика','экономика и бизнес', 'финансы ','события в своей стране', 'события в мире', 'спорт', 'красота и, стиль, мода', 'мир науки и техники', 'автомир', 'происшествия', 'отношения и психология', 'еда', 'карьера', 'жизнь звезд', 'тесты, гороскопы, гадания', 'животный мир', 'Эзотерика и экстросенсорика', 'Игры интеллектуальные', 'Игры компьютерные', 'История','эротика', 'поэзия и литература', 'религиозная тематика', 'старославянская тематика', 'йога', 'танцы', 'музыка', 'отдых и туризм, путешествия', 'реалити шоу', 'камеди-клаб', 'сериалы', 'фильмы', 'дети', 'художественное творчество и рукоделие', 'дача, приусадебное хозяйство', 'дом, квартира (уход, благоустройство и ремонт)', 'азартные игры по интернету', 'интернет-шопинг', 'учеба и самообразование', 'новое в медицине', 'новое в образовании', 'новое в законодательстве','общение с друзьями в соцсети'];
         $scope.testName = "tv_shows_preferences";
         $scope.testDescription = "Цель применения теста: выявление индивидуально-типологических различий предпочтения рубрик передач. Инструкция проведения теста. Просмотрите сначала весь список рубрик. Выберите по пять наиболее (+) и наименее (–) интересных рубрик новостей, информации и передач в интернете или на телевиденье и отметьте напротив каждой (из 43) уровень Вашего интереса к ней.";
@@ -21,6 +21,125 @@
         }];
         $scope.modalInstance = $uibModalInstance;
         $scope.content = CONSTANTS.STRING;
+
+        $scope.outputData = outputData;
+
+        function outputData(selectedColors, selectedPosButton, selectedNegButton) {
+            var T20 = preferencesService.outputDataFormat(selectedColors, selectedPosButton, selectedNegButton);
+            var TT20 = [[],[]];
+            for (var j = 44; j<48; j++) {
+                if (T20[j] === 1 && T20[1]>0) {
+                    TT20[15] = TT20[15] + T20[1] + 1;
+                    TT20[17] = TT20[17] + T20[1] + 2;
+                } else if (T20[j] == 2 && T20[2] > 0) {
+                    TT20[14] = TT20[14] + T20[2] + 2;
+                    TT20[15] = TT20[15] + T20[2] + 1;
+                    TT20[16] = TT20[16] + T20[2] + 1;
+                } else if (T20[j] == 3 && T20[3] > 0) {
+                    TT20[14] = TT20[14] + T20[3] + 2;
+                    TT20[15] = TT20[15] + T20[3] + 1;
+                    TT20[12] = TT20[12] + T20[3] + 1;
+                } else if (T20[j] == 4 && T20[4] > 0) {
+                    TT20[17] = TT20[17] + T20[4] + 1;
+                    TT20[5] = TT20[5] + T20[4] + 1;
+                    TT20[15] = TT20[15] + T20[4] + 1;
+                    TT20[9] = TT20[9] + T20[4] + 2;
+                    TT20[7] = TT20[7] + T20[4] + 1;
+                    TT20[3] = TT20[3] + T20[4] + 1;
+                    TT20[18] = TT20[18] + T20[4] + 1;
+                } else if (T20[j] == 5 && T20[5] > 0) {
+                    TT20[17] = TT20[17] + T20[5] + 1;
+                    TT20[18] = TT20[18] + T20[5] + 1;
+                    TT20[14] = TT20[14] + T20[5] + 1;
+                    TT20[11] = TT20[11] + T20[5] + 1;
+                    TT20[9] = TT20[9] + T20[5] + 2;
+                    TT20[7] = TT20[7] + T20[5] + 1;
+                    TT20[5] = TT20[5] + T20[5] + 1;
+                    TT20[4] = TT20[4] + T20[5] + 1;
+                } else if (T20[j] == 6 && T20[6] > 0) {
+                    TT20[11] = TT20[11] + T20[6] + 2;
+                    TT20[5] = TT20[5] + T20[6] + 1;
+                    TT20[3] = TT20[3] + T20[6] + 1;
+                } else if (T20[j] == 7 && T20[7] > 0) {
+                    TT20[7] = TT20[7] + T20[7] + 2;
+                    TT20[5] = TT20[5] + T20[7] + 1;
+                    TT20[3] = TT20[3] + T20[7] + 1;
+                } else if (T20[j] == 8 && T20[8] > 0) {
+
+                } else if (T20[j] == 9 && T20[9] > 0) {
+
+                } else if (T20[j] == 10 && T20[10] > 0) {
+
+                } else if (T20[j] == 11 && T20[11] > 0) {
+
+                } else if (T20[j] == 12 && T20[12] > 0) {
+
+                } else if (T20[j] == 13 && T20[13] > 0) {
+
+                } else if (T20[j] == 14 && T20[14] > 0) {
+
+                } else if (T20[j] == 15 && T20[15] > 0) {
+
+                } else if (T20[j] == 16 && T20[16] > 0) {
+
+                } else if (T20[j] == 17 && T20[17] > 0) {
+
+                } else if (T20[j] == 18 && T20[18] > 0) {
+
+                } else if (T20[j] == 19 && T20[19] > 0) {
+
+                } else if (T20[j] == 20 && T20[20] > 0) {
+
+                } else if (T20[j] == 21 && T20[21] > 0) {
+
+                } else if (T20[j] == 22 && T20[22] > 0) {
+
+                } else if (T20[j] == 23 && T20[23] > 0) {
+
+                } else if (T20[j] == 24 && T20[24] > 0) {
+
+                } else if (T20[j] == 25 && T20[25] > 0) {
+
+                } else if (T20[j] == 26 && T20[26] > 0) {
+
+                } else if (T20[j] == 27 && T20[27] > 0) {
+
+                } else if (T20[j] == 28 && T20[28] > 0) {
+
+                } else if (T20[j] == 29 && T20[29] > 0) {
+
+                } else if (T20[j] == 30 && T20[30] > 0) {
+
+                } else if (T20[j] == 31 && T20[31] > 0) {
+
+                } else if (T20[j] == 32 && T20[32] > 0) {
+
+                } else if (T20[j] == 33 && T20[33] > 0) {
+
+                } else if (T20[j] == 34 && T20[34] > 0) {
+
+                } else if (T20[j] == 35 && T20[35] > 0) {
+
+                } else if (T20[j] == 36 && T20[36] > 0) {
+
+                } else if (T20[j] == 37 && T20[37] > 0) {
+
+                } else if (T20[j] == 38 && T20[38] > 0) {
+
+                } else if (T20[j] == 39 && T20[39] > 0) {
+
+                } else if (T20[j] == 40 && T20[40] > 0) {
+
+                } else if (T20[j] == 41 && T20[41] > 0) {
+
+                } else if (T20[j] == 42 && T20[42] > 0) {
+
+                } else if (T20[j] == 43 && T20[43] > 0) {
+
+                }
+            }
+        }
+
     }
 
 })();
